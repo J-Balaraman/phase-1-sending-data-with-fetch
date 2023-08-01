fd9dd4a93930356a9df7761c725fa694155a15e9
jbalaraman <james.balaraman@gmail.com>
2023-08-01 16:35:04 -0600

    Those last two tests got me

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+
+function submitData(names, emails) {
+  return  fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    name: `${names}`,
+    email: `${emails}`,
+  }),
+})
+.then((resp) => resp.json())
+
+.then((data) => {
+  document.body.innerHTML = data.id
+})
+
+.catch((error) => document.body.innerHTML = error.message)
+}
